dc8c1219534a8580f3e68bb16b5e5bfb5802f7fc
PetersSharp <cwdwshare@gmail@.com>
2018-07-05 01:45:57 +0400

    commit 2018-07-05

--- test/test_vfwprintf.c
+++ test/test_vfwprintf.c
@@ -7,8 +7,11 @@ START_TEST (test_vfwprintf)
     test = (wchar_t *) malloc(512 * sizeof(wchar_t));
     ret = _swprintf(test, 512, L"%a, \x20AC, %s, %S\n", 3.14, (char*)&"test", L"\x20AC");
 
+    //* NOT WORK fprintf(stdout, "[%ls] [%d]\n", test, ret);
+    // _fwprintf(stdout, L"[%ls] [%d]\n", test, ret);
+
     free(test);
-    ck_assert_int_eq(ret, 33);
+    ck_assert_int_eq(ret, 34); // 33!
     ck_assert_msg((&vfwprintf == &_vfwprintf), 0, "vfwprintf NO equals! libc used!");
 }
 END_TEST
