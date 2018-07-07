f6b1257b5c69174aa8cf7cbee827b8e8e882d9e2
PetersSharp <cwdwshare@gmail@.com>
2018-07-07 20:27:56 +0400

    commit 2018-07-07

--- test/test_vfwprintf.c
+++ test/test_vfwprintf.c
@@ -6,13 +6,13 @@ START_TEST (test_vfwprintf)
 
     test = (wchar_t *) malloc(512 * sizeof(wchar_t));
     ret = _swprintf(test, 512, L"%a, \x20AC, %s, %S\n", 3.14, (char*)&"test", L"\x20AC");
-    //ck_assert_int_eq(ret, (int)_wcslen(test));
     ck_assert_int_eq(ret, 33);
     len = _wcslen(test);
+    ck_assert_int_eq(ret, len);
 
-    _fprintf(stdout, "\tTest _vfprintf:%d -> [%d/%d] = [%ls]\n", __LINE__, ret, len, test);
-    _fwprintf(stdout, L"\tTest _vfwprintf:%d -> [%d/%d] = [%ls]\n", __LINE__, ret, len, test);
-    _fwprintf(stdout, L"\tTest _vfwprintf:%d -> [%d/%d] = [%S]\n", __LINE__, ret, len, test);
+    _fprintf(stdout, "\tTest _vfprintf:%d -> [%d/%d] = %ls\n", __LINE__, ret, len, test);
+    _fwprintf(stdout, L"\tTest _vfwprintf:%d -> [%d/%d] = %ls\n", __LINE__, ret, len, test);
+    _fwprintf(stdout, L"\tTest _vfwprintf:%d -> [%d/%d] = %S\n", __LINE__, ret, len, test);
 
     free(test);
     ck_assert_msg((&vfwprintf == &_vfwprintf), 0, "vfwprintf NO equals! libc used!");
