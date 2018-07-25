0effb7cb2a0dd2ab8554cd4c9cfa47415e98bcff
PetersSharp <cwdwshare@gmail@.com>
2018-07-25 17:26:55 +0400

    docs update 2018-07-25

--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['u8stowcs',['u8stowcs',['../wchar2_8h.html#ae94a562566dbe283cabb43d34f0ba08a',1,'wchar2.h']]],
   ['u8sverify',['u8sverify',['../wchar2_8h.html#a0f3a207d66dff9f6d0a3187b22931407',1,'wchar2.h']]],
   ['u8waccess',['u8waccess',['../wchar2_8h.html#a155238aa5ff7757120bc1218563bb71d',1,'wchar2.h']]],
-  ['u8wfopen',['u8wfopen',['../wchar2_8h.html#a1593c26b329c3e552f39684f77be3236',1,'wchar2.h']]],
+  ['u8wfopen',['u8wfopen',['../wchar2_8h.html#a0a0616a33fd410ab523d044b676c9e76',1,'wchar2.h']]],
   ['u8wmkdir',['u8wmkdir',['../wchar2_8h.html#a5078390fceefa42d2f51080d636b20cb',1,'wchar2.h']]],
   ['u8wpathnormalize',['u8wpathnormalize',['../wchar2_8h.html#a03890d8977a21209211136beb3f314f1',1,'wchar2.h']]],
   ['u8wremove',['u8wremove',['../wchar2_8h.html#ad8dcd1716a3a33693b64d1af4cd4c15a',1,'wchar2.h']]],
