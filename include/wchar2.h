7017039fdca9f0dd9e56170bab6312abf5717b02
PetersSharp <cwdwshare@gmail@.com>
2018-07-31 11:38:37 +0400

    commit 2018-07-31

--- include/wchar2.h
+++ include/wchar2.h
@@ -778,7 +778,24 @@ int       _fputws(const wchar_t *restrict, FILE *restrict);
 wchar_t   _fputwc(wchar_t, FILE *restrict);
 
           /*!
-           * \includedoc docs/doxdesc/_wcsftime.dox
+           * \~English
+           * \brief Convert date and time to a formatted wide-character string.
+           *
+           * \details The original version of the `strftime ()` function is used for the conversion.
+           *          The `wcsftime()` function converts the results from wide characters to a multibyte
+           *          string and back.
+           *
+           * \~Russian
+           * \brief Преобразование даты и времени в форматированную строку с широкими символоми.
+           *
+           * \details Это широкосимвольный аналог `strftime()`.
+           *          Для преобразования используется оригинальная версия функции `strftime()`.
+           *          Функция преобразовывает результаты из строки широких символов в многобайтовую
+           *          строку и обратно.
+           *
+           * \~
+           * - \subpage wcsftime
+           * - \ref wcsptime related `wcsptime(..)`
            */
 size_t    _wcsftime(wchar_t *restrict, size_t sz, const wchar_t *restrict, const void *restrict);
 
